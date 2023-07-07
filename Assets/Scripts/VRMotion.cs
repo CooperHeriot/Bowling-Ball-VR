bbd5380082667c47096b92d9b83742083ab38055
CooperHeriot <107088336+CooperHeriot@users.noreply.github.com>
2023-07-07 11:24:36 +1000

    sderft

---
+++ Assets/Scripts/VRMotion.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class VRMotion : MonoBehaviour
+{
+    public XRNode inputSource;
+    private XROrigin rig;
+
+    private Vector2 inputAxis;
+    private CharacterController character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
